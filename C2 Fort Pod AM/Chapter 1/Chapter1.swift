2ada9f3dcec746fc986cd19719a4e1b9e688e575
Cory Tripathy <ctripathy0@gmail.com>
2023-10-20 10:34:00 -0400

    moved story to chapter 1

--- C2 Fort Pod AM/Chapter 1/Chapter1.swift
+++ C2 Fort Pod AM/Chapter 1/Chapter1.swift
@@ -6,5 +6,23 @@
 import Foundation
 
 func chapterOne() {
-    // Your portion of the story goes here
+    var dayOfTheWeek = "Friday"
+    let isSick = false
+    
+    // on fridays, i always drink a cup of coffee. even if im sick
+    
+    if isSick {
+        print("Cory was sick. he didnt go to work")
+        if dayOfTheWeek == "Friday" {
+            print("cory got a cup of coffee")
+        }
+    } else if dayOfTheWeek == "Monday" {
+        print("cory was late to work")
+        print("cory missed the mentor huddle")
+    } else {
+        if dayOfTheWeek == "Friday" {
+            print("cory got a cup of coffee")
+        }
+        print("cory was barely on time to work")
+    }
 }
--- C2 Fort Pod AM/main.swift
+++ C2 Fort Pod AM/main.swift
@@ -14,40 +14,4 @@ func readStory() {
 }
 
 readStory()
-
-
-
-var dayOfTheWeek = "Friday"
-let isSick = false
-
-// on fridays, i always drink a cup of coffee. even if im sick
-
-if isSick {
-    print("Cory was sick. he didnt go to work")
-    if dayOfTheWeek == "Friday" {
-        print("cory got a cup of coffee")
-    }
-} else if dayOfTheWeek == "Monday" {
-    print("cory was late to work")
-    print("cory missed the mentor huddle")
-} else {
-    if dayOfTheWeek == "Friday" {
-        print("cory got a cup of coffee")
-    }
-    print("cory was barely on time to work")
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+
