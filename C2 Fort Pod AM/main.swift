3f6d4b14120e6e9f20f27af5690414579d813946
Cory Tripathy <ctripathy0@gmail.com>
2023-10-20 10:25:45 -0400

    Initial Commit

---
+++ C2 Fort Pod AM/main.swift
@@ -0,0 +1,53 @@
+//
+//  main.swift
+//  CLIStoryTemplate
+//
+
+import Foundation
+
+func readStory() {
+    chapterOne()
+    chapterTwo()
+    chapterThree()
+    chapterFour()
+    chapterFive()
+}
+
+readStory()
+
+
+
+var dayOfTheWeek = "Friday"
+let isSick = false
+
+// on fridays, i always drink a cup of coffee. even if im sick
+
+if isSick {
+    print("Cory was sick. he didnt go to work")
+    if dayOfTheWeek == "Friday" {
+        print("cory got a cup of coffee")
+    }
+} else if dayOfTheWeek == "Monday" {
+    print("cory was late to work")
+    print("cory missed the mentor huddle")
+} else {
+    if dayOfTheWeek == "Friday" {
+        print("cory got a cup of coffee")
+    }
+    print("cory was barely on time to work")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
