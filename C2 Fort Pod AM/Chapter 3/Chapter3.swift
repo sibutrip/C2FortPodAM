46e17c1c1a3a311fd1e1cee4bf8793c6305b06a3
Cory Tripathy <ctripathy0@gmail.com>
2023-10-30 10:35:20 -0400

    added ch 3

--- C2 Fort Pod AM/Chapter 3/Chapter3.swift
+++ C2 Fort Pod AM/Chapter 3/Chapter3.swift
@@ -6,5 +6,17 @@
 import Foundation
 
 func chapterThree() {
-    // Your portion of the story goes here
+    // Javi fell into a black hole
+    // because of this... he was taken to a different dimension
+    // because of this... he befriended a variety of multidimensional vegetables
+    
+    var multidimensionalVegetables: [String] = ["Carrot",
+                                                "Cucumber",
+                                                "Squash"]
+    
+    print("\(protagonistName) met a interesting cast of characters including \(multidimensionalVegetables[0]), \(multidimensionalVegetables[1]), and \(multidimensionalVegetables[2])")
+    
+    print("\(protagonistName) tries to a find a way back home, and in the process he got hungry and ate \(multidimensionalVegetables.remove(at: 2))")
+    print("now there are only \(multidimensionalVegetables.count) remaining")
+    
 }
